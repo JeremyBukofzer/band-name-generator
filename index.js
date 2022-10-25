1499356cb609966e38b991b02c66375d071dfb6f
Jeremy Bukofzer <jlbukofzer@gmail.com>
2022-10-24 21:21:53 -0500

    adds generator and text animation

---
+++ index.js
@@ -0,0 +1,11 @@
+import chalkAnimation from 'chalk-animation';
+
+import { generateSlug } from 'random-word-slugs'
+
+
+const length = parseInt(process.argv[2])
+
+const bandName = generateSlug(length, { format: "title" })
+
+
+chalkAnimation.rainbow(bandName)
